f39a1cbcffacc2be3c6d954020b71b4bf09ca124
soncii <Damir0110gim@gmail.com>
2022-11-03 21:30:27 +0600

    cors fix

--- src/main/java/com/me/DenSys/app/RestControl.java
+++ src/main/java/com/me/DenSys/app/RestControl.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin()
-@Controller
+@RestController
 public class RestControl {
     @Autowired
     PatientRepository patientRepository;
