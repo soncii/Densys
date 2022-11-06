7051ad18d73839e9504bfb34e19decc99e1749eb
soncii <Damir0110gim@gmail.com>
2022-11-06 13:06:33 +0600

    select impl

--- src/main/java/com/me/DenSys/app/RestControl.java
+++ src/main/java/com/me/DenSys/app/RestControl.java
@@ -1,7 +1,6 @@
 package com.me.DenSys.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,28 +16,24 @@ public class RestControl {
     @PostMapping(path="/add/patient",
     consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity addPatient(@RequestBody Patient newPatient) {
-        System.out.println("I'm trying to add");
+//        System.out.println("I'm trying to add");
+        Patient Damir = new Patient();
+        System.out.println(Damir.getId());
         patientRepository.save(newPatient);
-        return ResponseEntity.ok(HttpStatus.OK);
+        return ResponseEntity.ok(Damir.getId());
     }
     @GetMapping(path="/see/patients",
     produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> requestAllPatients() {
-        //JSONObject res = new JSONObject();
         List<Patient> allPatients = patientRepository.findAll();
         return ResponseEntity.ok(allPatients);
-
     }
     @GetMapping(path="/see/patient/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> patientById(@PathVariable Long id) {
-        //JSONObject res = new JSONObject();
         Optional<Patient> byId = patientRepository.findById(id);
         if (byId.isPresent()) return ResponseEntity.ok(byId);
-        return ResponseEntity.notFound().build();
-
-
-
+        return ResponseEntity.status(404).build();
     }
     @GetMapping("/login")
     public String Home(){
