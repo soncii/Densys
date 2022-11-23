8b8f12807d28a92a63520e939fc6bf6027494f63
soncii <Damir0110gim@gmail.com>
2022-11-23 18:18:31 +0600

    ScheduleDetails

--- src/main/java/com/me/DenSys/app/entities/ScheduleDetails.java
+++ src/main/java/com/me/DenSys/app/entities/ScheduleDetails.java
@@ -22,5 +22,46 @@ public class ScheduleDetails {
 
     @Column LocalTime end;
 
+    public ScheduleDetails() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDoctorEmail() {
+        return doctorEmail;
+    }
+
+    public void setDoctorEmail(String doctorEmail) {
+        this.doctorEmail = doctorEmail;
+    }
+
+    public Integer getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(Integer weekDay) {
+        this.weekDay = weekDay;
+    }
+
+    public LocalTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalTime start) {
+        this.start = start;
+    }
+
+    public LocalTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalTime end) {
+        this.end = end;
+    }
 }
