982a9160a29ded346c1980071fd3215326065966
soncii <Damir0110gim@gmail.com>
2022-11-23 18:47:01 +0600

    ScheduleDetails

--- src/main/java/com/me/DenSys/app/AppApplication.java
+++ src/main/java/com/me/DenSys/app/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
-@EntityScan(basePackages = {"com.me.DenSys.app.entities."})  // scan JPA entities
+@EntityScan(basePackages = {"com.me.DenSys.app.entities"})  // scan JPA entities
 public class AppApplication {
 	//comment by me nnd  bb
 	public static void main(String[] args) {
