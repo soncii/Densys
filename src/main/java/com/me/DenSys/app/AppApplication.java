9ea54b5acddcb1e9a60c6972001736436df46d8a
m_ala <alan.mukeyev@nu.edu.kz>
2022-11-23 08:54:35 +0000

    my first nn bb

--- src/main/java/com/me/DenSys/app/AppApplication.java
+++ src/main/java/com/me/DenSys/app/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 public class AppApplication {
-	//comment by me nnd
+	//comment by me nnd  bb
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
