a49494b5240388fb88a294a84ac099161d5b7af2
soncii <Damir0110gim@gmail.com>
2022-11-30 23:52:25 +0600

    ScheduleDetails

--- src/main/java/com/me/DenSys/app/repositories/DoctorRepository.java
+++ src/main/java/com/me/DenSys/app/repositories/DoctorRepository.java
@@ -2,14 +2,17 @@ package com.me.DenSys.app.repositories;
 
 import com.me.DenSys.app.entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @Repository(value = "Dam")
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
     List<Doctor> findAllBySpecializationId(String spec);
-//    Optional<Doctor> find(String iin, String password);
+    @Query(nativeQuery = true, value = "Select * from doctor d where d.iin=:iin and d.password=:password")
+    Optional<Doctor> findNative(String iin, String password);
 }
