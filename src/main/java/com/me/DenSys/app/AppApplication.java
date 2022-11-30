0cb0a1d86b6a865b353f3dfafc87d87d963ce1f4
soncii <Damir0110gim@gmail.com>
2022-12-01 01:10:17 +0600

    image

--- src/main/java/com/me/DenSys/app/AppApplication.java
+++ src/main/java/com/me/DenSys/app/AppApplication.java
@@ -2,8 +2,10 @@ package com.me.DenSys.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
+@EnableWebMvc
 //@EntityScan(basePackages = {"com.me.DenSys.app.entities"})  // scan JPA entities
 public class AppApplication {
 	//comment by me nnd  bb
